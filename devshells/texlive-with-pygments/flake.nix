e86768299c191be27890cbb09e3869f444205ad7
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-02-18 12:00:41 +0200

    Add compatability for zsh to gitprompt
    This helps with the shell shifting the cursor around

---
+++ devshells/texlive-with-pygments/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Flake for TeXlive with python pygments";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell { packages = with pkgs; [ texliveFull python3Packages.pygments ]; };
+      }
+    );
+}
--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -1,4 +1,4 @@
-{config, pkgs, pkgs-unstable, ...}: {
+{lib, config, pkgs, pkgs-unstable, ...}: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -23,7 +23,7 @@
 
     initExtra = ''
       setopt promptsubst
-      export PROMPT='%F{12}[%f%F{10}%n%f%F{12}@%f%F{white}%m%f%F{12}]%f%F{white}:%f %F{white}%~%f%F{12}>%b$(${pkgs-unstable.gitprompt-rs}/bin/gitprompt-rs)%f%F{10}%(!.#.$)%f '
+      export PROMPT='%F{12}[%f%F{10}%n%f%F{12}@%f%F{white}%m%f%F{12}]%f%F{white}:%f %F{white}%~%f%F{12}>%b$(${lib.getExe pkgs-unstable.gitprompt-rs} zsh)%f%F{10}%(!.#.$)%f '
       unsetopt beep
       bindkey '^R' history-incremental-search-backward
     '';

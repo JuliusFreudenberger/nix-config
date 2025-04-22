ad15247cae832b1731f2ef80cfde3462c050c8ad
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-04-22 18:46:20 +0300

    Add more mimeapps

--- home/xdg/default.nix
+++ home/xdg/default.nix
@@ -8,8 +8,11 @@
         "image/svg+xml" = ["viewnior.desktop"];
         "image/jpeg" = ["viewnior.desktop"];
         "image/png" = ["viewnior.desktop"];
+        "image/tiff" = ["viewnior.desktop"];
+        "video/quicktime" = ["vlc.desktop"]; # *.mov
         "application/yaml" = ["org.x.editor.desktop"];
         "text/plain" = ["org.x.editor.desktop"];
+        "text/calendar" = ["thunderbird.desktop"];
       };
     };
     userDirs.enable = true;

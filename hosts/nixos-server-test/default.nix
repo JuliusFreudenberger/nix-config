2f19307456d659f10326d4be8ad9e6ab20405c96
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-08-12 02:27:42 +0200

    Configure proxmox-nixos cache

--- hosts/nixos-server-test/default.nix
+++ hosts/nixos-server-test/default.nix
@@ -34,6 +34,15 @@
     };
   };
 
+  nix.settings = {
+    substituters = [
+      "https://cache.saumon.network/proxmox-nixos"
+    ];
+    trusted-public-keys = [
+      "proxmox-nixos:D9RYSWpQQC/msZUWphOY2I5RLH5Dd6yQcaHIuug7dWM="
+    ];
+  };
+
   services.proxmox-ve = {
     enable = true;
     ipAddress = "192.168.122.71";

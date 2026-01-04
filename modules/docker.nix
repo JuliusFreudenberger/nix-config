ed21c242620c6348fb9ce5555a08925791ed256b
JuliusFreudenberger <julius+git@jfreudenberger.de>
2026-01-04 22:17:40 +0100

    Enable ipv6 in docker

--- modules/docker.nix
+++ modules/docker.nix
@@ -7,6 +7,10 @@
   virtualisation = {
     docker = {
       enable = true;
+      daemon.settings = {
+        ipv6 = true;
+        ip6tables = true;
+      };
     };
     oci-containers.backend = "docker";
   };

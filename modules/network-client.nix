edbde9800623dc6bb2c6e6a3f7485755e80cb5c2
JuliusFreudenberger <julius+git@jfreudenberger.de>
2026-03-11 23:13:30 +0100

    Remove firewall rules for wireguard and set rpfilter to loose

--- modules/network-client.nix
+++ modules/network-client.nix
@@ -13,19 +13,7 @@
     firewall = {
       # if packets are still dropped, they will show up in dmesg
       logReversePathDrops = true;
-      # wireguard trips rpfilter up
-      extraCommands = ''
-        iptables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 1194 -j RETURN
-        ip6tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 1194 -j RETURN
-        iptables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 1194 -j RETURN
-        ip6tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 1194 -j RETURN
-      '';
-      extraStopCommands = ''
-        iptables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 1194 -j RETURN || true
-        ip6tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 1194 -j RETURN || true
-        iptables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 1194 -j RETURN || true
-        ip6tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 1194 -j RETURN || true
-      '';
+      checkReversePath = "loose";
     };
   };
 }

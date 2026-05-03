7a7901ab59a0987d63b94a01ef936aa407a4368a
JuliusFreudenberger <julius+git@jfreudenberger.de>
2026-05-04 01:49:50 +0200

    Add DBeaver as lazy app

--- modules/gui-coding.nix
+++ modules/gui-coding.nix
@@ -11,6 +11,21 @@
     zed-editor.fhs
 
     k6
+
+    (pkgs.lazy-app.override {
+      pkg = pkgs.dbeaver-bin;
+      desktopItem = pkgs.makeDesktopItem {
+        name = "DBeaver";
+        exec = "env NO_AT_BRIDGE=1 dbeaver %U";
+        icon = "dbeaver";
+        desktopName = "DBeaver";
+        comment = "SQL Integrated Development Environment";
+        categories = [ "IDE" "Development" ];
+        mimeTypes = [
+          "application/sql"
+        ];
+      };
+    })
   ];
 
 }

cb17bea00c796edd2e53db5bb627c5eab7348b40
JuliusFreudenberger <julius+git@jfreudenberger.de>
2026-06-26 22:03:02 +0200

    Configure xdg portals for sway

--- modules/sway.nix
+++ modules/sway.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   ...
 }: {
   programs.sway = {
@@ -8,6 +9,21 @@
       gtk = true;
     };
   };
-  xdg.portal.wlr.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr = {
+      enable = true;
+      settings = {
+        screencast = {
+          chooser_type = "simple";
+          chooser_cmd = "${pkgs.slurp}/bin/slurp -f 'Select: %o' -or";
+        };
+      };
+    };
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+  };
 }
 

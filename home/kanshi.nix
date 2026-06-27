c2d242301c3cd9bee9117a2d16afcd1f2fbb64aa
JuliusFreudenberger <julius+git@jfreudenberger.de>
2026-06-27 10:57:14 +0200

    Add m15 configuration to kanshi

--- home/kanshi.nix
+++ home/kanshi.nix
@@ -1,13 +1,15 @@
 {
   ...
 }: let
-  t27 = "Lenovo Group Limited T27h-30 V5HGY026";
+  t27 = "Lenovo Group Limited T27h-30*";
+  m15 = "Lenovo Group Limited M15*";
 in {
   services.kanshi = {
     enable = true;
     settings = [
       { output.criteria = "eDP-1"; output.scale = 1.0; }
       { output.criteria = t27; }
+      { output.criteria = m15; }
       { profile.name = "mobile";
         profile.outputs = [
           { criteria = "eDP-1"; }
@@ -17,6 +19,12 @@ in {
         profile.outputs = [
           { criteria = "eDP-1"; position = "152,1440"; }
           { criteria = t27; position = "0,0"; }
+        ];
+      }
+      { profile.name = "m15" ;
+        profile.outputs = [
+          { criteria = "eDP-1"; position = "0,1080"; }
+          { criteria = m15; position = "168,0"; }
         ];
       }
     ];
